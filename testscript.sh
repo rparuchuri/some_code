404abde6a7e9a651af6442ed76352f96f354e27e
rparuchuri <paruchuri.renukadevi@gmail.com>
2019-08-06 21:59:54 -0400

    fix typo

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,4 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "This is completed"
-echo " This was run automagically"
+echo " This was run automamically"
